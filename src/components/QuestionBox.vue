513343ba9b105d9d9b04d92a05301296e957abc7
adrian <cr7adrian@hotmail.com>
2019-08-23 17:43:46 -0400

    Enter/Leave transition added to the component

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,20 +1,60 @@
 <template>
   <div>
     <b-jumbotron>
-      <template slot="header">BootstrapVue</template>
-
       <template slot="lead">
-        Some Question here!
+        <div>
+          <transition name="fade">
+            <p v-if="show" :key="currentQuestion.question">{{currentQuestion.question}}</p>
+          </transition>
+        </div>
       </template>
 
       <hr class="my-4" />
 
-      <p>
-        List of Answers
-      </p>
+      <p>List of Answers</p>
 
       <b-button variant="primary" href="#">Do Something</b-button>
-      <b-button variant="success" href="#">Next</b-button>
+      <b-button variant="success" href="#" v-on:click="forceRerender()" @click="index">Next</b-button>
     </b-jumbotron>
   </div>
 </template>
+
+<script>
+// Globally
+import Vue from "vue";
+
+export default {
+  data() {
+    return {
+      show: true
+    };
+  },
+
+  methods: {
+    forceRerender: function() {
+      this.show = false;
+      setTimeout(
+        function() {
+          this.show = true;
+        }.bind(this),
+        500
+      );
+    }
+  },
+
+  props: {
+    currentQuestion: Object,
+    index: Function
+  }
+};
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>

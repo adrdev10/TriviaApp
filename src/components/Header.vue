19297da7d87cb788636312abf26831990ffa6042
adrian <cr7adrian@hotmail.com>
2019-08-23 11:45:27 -0400

    Init project

---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div>
+      <b-nav>
+        <b-nav-item active>Trivia App</b-nav-item>
+        <b-nav-item>Counter: 4/10</b-nav-item>
+      </b-nav>
+    </div>
+  </div>
+</template>

2ec377bb1b83525cf37acd201e0b69ed9e66434b
adrian <cr7adrian@hotmail.com>
2019-08-23 13:10:55 -0400

    Boostrap jumbotron added to the UI

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,3 +1,20 @@
 <template>
-    <h1>Question Box</h1>
+  <div>
+    <b-jumbotron>
+      <template slot="header">BootstrapVue</template>
+
+      <template slot="lead">
+        Some Question here!
+      </template>
+
+      <hr class="my-4" />
+
+      <p>
+        List of Answers
+      </p>
+
+      <b-button variant="primary" href="#">Do Something</b-button>
+      <b-button variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
 </template>
